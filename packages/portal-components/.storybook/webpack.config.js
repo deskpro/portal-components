87d0a33e8a52e8a778ea238f1b64761eec63f535
Julien Ducro <julien.ducro@deskpro.com>
2018-04-16 17:11:24 +0200

    Add postcss-next

---
+++ packages/portal-components/.storybook/webpack.config.js
@@ -0,0 +1,32 @@
+var values = require('postcss-modules-values');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { modules: true, importLoaders: 1 } },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                values()
+              ],
+              sourceMap: true,
+            },
+          },
+        ]
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]',
+        },
+      }
+    ]
+  }
+};
