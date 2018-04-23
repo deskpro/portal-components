0568c9153f0924363f0e1cdd4a318eacbcdc4968
Julien Ducro <julien.ducro@deskpro.com>
2018-04-23 17:48:31 +0200

    POC with Formik

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,13 +1,62 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import Yup from 'yup';
+import { withFormik, Form, Field } from 'formik';
 
-import { Button, Welcome } from '@storybook/react/demo';
+const App = ({
+  values,
+  errors,
+  touched,
+  isSubmitting
+}) => (
+  <Form>
+    <div>
+      { touched.email && errors.email && <p>{errors.email}</p> }
+      <Field type="email" name="email" placeholder="Email"/>
+    </div>
+    <div>
+      { touched.password && errors.password && <p>{errors.password}</p> }
+      <Field type="password" name="password" placeholder="Password"/>
+    </div>
+    <label>
+      <Field type="checkbox" name="newsletter" checked={values.newsletter} />
+      Join our newsletter
+    </label>
+    <Field component="select" name="plan">
+      <option value="free">Free</option>
+      <option value="premium">Premium</option>
+    </Field>
+    <button disabled={isSubmitting}>Submit</button>
+  </Form>
+);
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+const FormikApp = withFormik({
+  mapPropsToValues({ email, password, newsletter, plan }) {
+    return {
+      email: email || '',
+      password: password || '',
+      newsletter: newsletter || false,
+      plan: plan || 'free'
+    }
+  },
+  validationSchema: Yup.object().shape({
+    email: Yup.string().email('Email not valid').required('Email is required'),
+    password: Yup.string().min(9, 'Password must be 9 characters or longer').required('Password is required')
+  }),
+  handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+    setTimeout(() => {
+      if(values.email === 'chris@deskpro.com') {
+        setErrors({ email: 'That email is already taken' })
+      } else {
+        resetForm()
+      }
+      setSubmitting(false)
+    }, 2000)
+  }
+})(App);
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf('Test Formik', module)
+  .add('POC', () =>
+    <FormikApp />
+  );
