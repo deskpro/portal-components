4717fa0ba33f85df8c21a162ec51905d1d0d628a
Julien Ducro <julien.ducro@deskpro.com>
2018-05-11 16:24:08 +0200

    Add Checkboxes

--- src/Components/Choices/Checkboxes.jsx
+++ src/Components/Choices/Checkboxes.jsx
@@ -1,48 +1,105 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { FieldArray, getIn } from 'formik';
 import Field from '../Field';
 
 class Checkboxes extends Field {
   constructor(props) {
     super(props);
-    this.type = 'checkbox';
+    this.state = {
+      focused: false,
+    };
   }
 
+  handleKeyPress = (e, form, arrayHelpers, value) => {
+    const { name } = this.props;
+    if (e.key === ' ') {
+      if (!form.values[name].includes(value)) {
+        arrayHelpers.push(value);
+      } else {
+        const idx = getIn(form.values, name).indexOf(value);
+        arrayHelpers.remove(idx);
+      }
+    }
+  };
+
+  handleBlur = () => {
+    this.setState({
+      focused: false
+    });
+  };
+
+  handleFocus = () => {
+    this.setState({
+      focused: true
+    });
+  };
+
+  renderLabel = () => {
+    const {
+      label,
+    } = this.props;
+    const htmlFor = this.id;
+    /* eslint-disable jsx-a11y/label-has-for */
+    return (
+      <label className="dp-pc_checkboxes_label" htmlFor={htmlFor}>
+        {label}
+      </label>
+    );
+    /* eslint-enable jsx-a11y/label-has-for */
+  };
   renderField = (form) => {
     const {
-      name, id, label, options, ...props
+      name, label, options, className, ...props
     } = this.props;
+    /* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+    /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
     return (
       <FieldArray
         name={name}
         render={arrayHelpers => (
-          <div {...props}>
+          <div
+            className={classNames('dp-pc_checkboxes', { focused: this.state.focused }, className)}
+            {...props}
+          >
             {options.map(option => (
-              <div key={option.value}>
-                <label htmlFor={`${this.id}-${option.value}`}>
-                  <input
-                    id={`${this.id}-${option.value}`}
-                    name={name}
-                    type="checkbox"
-                    value={option.value}
-                    checked={form.values[name].includes(option.value)}
-                    onChange={(e) => {
-                      if (e.target.checked) arrayHelpers.push(option.value);
-                      else {
-                        const idx = getIn(form.values, name).indexOf(option.value);
-                        arrayHelpers.remove(idx);
-                      }
-                    }}
-                  />{' '}
+              <label
+                key={option.value}
+                htmlFor={`${this.id}-${option.value}`}
+                tabIndex="0"
+                className="dp-pc_checkboxes_checkbox"
+                onKeyPress={e => this.handleKeyPress(e, form, arrayHelpers, option.value)}
+                onFocus={this.handleFocus}
+                onBlur={this.handleBlur}
+              >
+                <input
+                  id={`${this.id}-${option.value}`}
+                  name={name}
+                  type="checkbox"
+                  value={option.value}
+                  checked={form.values[name].includes(option.value)}
+                  onChange={(e) => {
+                    if (e.target.checked) arrayHelpers.push(option.value);
+                    else {
+                      const idx = getIn(form.values, name).indexOf(option.value);
+                      arrayHelpers.remove(idx);
+                    }
+                  }}
+                />
+                <i />
+                {' '}
+                <span className="label">
                   {option.label}
-                </label>
-              </div>
+                </span>
+              </label>
             ))}
           </div>
         )}
       />
     );
+    /* eslint-enable jsx-a11y/no-noninteractive-tabindex */
+    /* eslint-enable jsx-a11y/no-noninteractive-element-interactions */
   }
 }
 
@@ -52,6 +109,12 @@ Checkboxes.propTypes = {
     label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
     value: PropTypes.any.isRequired
   })),
+  className: PropTypes.string,
+};
+
+Checkboxes.defaultProps = {
+  ...Field.defaultProps,
+  className: ''
 };
 
 export default Checkboxes;
