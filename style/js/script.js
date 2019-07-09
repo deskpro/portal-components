ba154dac5a2bec4a42af84bcc76339d0a290720b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-09 18:23:02 +0000

    Deploy deskpro/portal-components to github.com/deskpro/portal-components.git:gh-pages

--- style/js/script.js
+++ style/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-	$('body').on('click', '.dp-po-dropdown-menu-item', function(event) {
+	$('body').on('click', '.dp-po-Dropdown-container .dp-po-dropdown-menu-item', function(event) {
 		var is_disabled = $(this).hasClass('is-disabled');
 		var is_selected = $(this).hasClass('is-selected');
-		console.log(is_disabled);
+		// console.log(is_disabled);
 		if(is_disabled == false){
 			if(is_selected == false){
 				
@@ -15,6 +15,7 @@ $(document).ready(function(){
 				$(this).closest('.dp-po-Dropdown-container').find('.dp-po-dropdown-atom').addClass('is-onselected');
 			}
 		}
+		event.preventDefault();
 	});
 	$('body').on('mouseover mousemove', '.subscribe--dropdown', function(event) {
 		if($(this).data('sitertohide')){
@@ -56,17 +57,6 @@ $(document).ready(function(){
 		$(this).toggleClass("is-active");
 	})
 
-	// 
-	$('body').on('click', '.addReplyComment', function(event) {
-		$(this).parents('.dp-po-comments-wrap .dp-po-comments-item + .dp-po-reply-comment').toggleClass('is-active')
-		$(this).parents('.dp-po-comments-wrap .dp-po-comments-item + .dp-po-reply-comment').slideToggle(200)
-	})
-	// 
-
-	$('body').on('click', '.dp-po-vote .dp-po-item-vote', function(event) {
-		$(this).toggleClass("is-active");
-	})
-
 	$('body').on('click', '.Button--success', function(event) {
 		$(this).parents('.Voting-section').removeClass('is-dislike')
 		$(this).parents('.Voting-section').addClass('is-like')
@@ -75,10 +65,37 @@ $(document).ready(function(){
 		$(this).parents('.Voting-section').removeClass('is-like')
 		$(this).parents('.Voting-section').addClass('is-dislike')
 	})
+	
+	// Reply comment
 
+	$('body').on('click', '.addReplyComment', function(event) {
+		$('.dp-po-comments-item').find('.dp-po-comments-wrap.dp-po-reply-comment').remove();
+		if($(this).closest('.dp-po-comments-item').find('.dp-po-comments-wrap.dp-po-reply-comment').length < 1){
+			console.log($('body').find('.dp-po-comments-wrap.dp-po-reply-comment').length);
+			var clone = $('body').find('.dp-po-comments-wrap.dp-po-reply-comment:eq(0)').clone();
+			var item_parent = $(this).closest('.dp-po-comments-item');
+			clone.appendTo(item_parent);
+		}
+		$(this).closest('.dp-po-comments-item').find('.dp-po-comments-wrap.dp-po-reply-comment').addClass('is-active');
+		$(this).parents('.Voting-section').addClass('is-dislike');
+		$(this).closest('.dp-po-label').find('.Icon--Dots').removeClass('is-active');
+		$('body').find('.dp-po-comments.dp-po-add-comments').nextAll('.dp-po-comments-wrap').slideUp(200);
+		$('body').find('.dp-po-comments.dp-po-add-comments').removeClass('is-active');
+		$('body').find('.dp-po-comments.dp-po-add-comments').addClass('is-disabled');
+		return false;
+	})
+
+	$('body').on('click', '.dp-po-comments-wrap.dp-po-reply-comment .dp-po-reply-comment-close', function(event) {
+		$(this).closest('.dp-po-comments-wrap.dp-po-reply-comment').removeClass('is-active');
+		$('body').find('.dp-po-comments.dp-po-add-comments').nextAll('.dp-po-comments-wrap').slideDown(200);
+		$('body').find('.dp-po-comments.dp-po-add-comments').addClass('is-active');
+		$('body').find('.dp-po-comments.dp-po-add-comments').removeClass('is-disabled');
+	})
 	$('body').on('click', '.dp-po-comments.dp-po-add-comments', function(event) {
-		$(this).toggleClass('is-active');
-		$(this).nextAll('.dp-po-comments-wrap').slideToggle(200)
+		if(!$(this).hasClass('is-disabled')){
+			$(this).toggleClass('is-active');
+			$(this).nextAll('.dp-po-comments-wrap').slideToggle(200);
+		}
 	})
 
 	$('body').on('click', '.dp-po-sort', function(event) {
@@ -99,19 +116,19 @@ function filterView_subscribe(){
 
 // cut function Share btn
 
-function filterView_share(){
-    if (window.matchMedia('(max-width: 767px)').matches) {
-        $('.mobileCutShare .Dropdown-share').detach().appendTo('.dp-po-col-9 .mobileCutBtn');
-    }else{  
-        $('.mobileCutBtn .Dropdown-share').detach().prependTo('.Sidebar--right .mobileCutShare');
-    }
-}
+// function filterView_share(){
+//     if (window.matchMedia('(max-width: 767px)').matches) {
+//         $('.mobileCutShare .Dropdown-share').detach().appendTo('.dp-po-col-9 .mobileCutBtn');
+//     }else{  
+//         $('.mobileCutBtn .Dropdown-share').detach().prependTo('.Sidebar--right .mobileCutShare');
+//     }
+// }
 
 $(document).ready(function() {
     filterView_subscribe();
-    filterView_share();
+    // filterView_share();
     $(window).resize(function() {
         filterView_subscribe();
-        filterView_share();
+        // filterView_share();
     });
 });
