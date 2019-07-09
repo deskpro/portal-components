f24bfd79301efd4ac3eab8e1ce3c98f10d315ca6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-09 13:12:01 +0000

    Deploy deskpro/portal-components to github.com/deskpro/portal-components.git:gh-pages

--- style/js/script.js
+++ style/js/script.js
@@ -52,6 +52,21 @@ $(document).ready(function(){
 		$(this).data('sitertohide',timer);
 	})
 
+	$('body').on('click', '.label--time .Icon--Dots', function(event) {
+		$(this).toggleClass("is-active");
+	})
+
+	// 
+	$('body').on('click', '.addReplyComment', function(event) {
+		$(this).parents('.dp-po-comments-wrap .dp-po-comments-item + .dp-po-reply-comment').toggleClass('is-active')
+		$(this).parents('.dp-po-comments-wrap .dp-po-comments-item + .dp-po-reply-comment').slideToggle(200)
+	})
+	// 
+
+	$('body').on('click', '.dp-po-vote .dp-po-item-vote', function(event) {
+		$(this).toggleClass("is-active");
+	})
+
 	$('body').on('click', '.Button--success', function(event) {
 		$(this).parents('.Voting-section').removeClass('is-dislike')
 		$(this).parents('.Voting-section').addClass('is-like')
@@ -61,9 +76,42 @@ $(document).ready(function(){
 		$(this).parents('.Voting-section').addClass('is-dislike')
 	})
 
-
-	$('body').on('click', '.dp-po-comments', function(event) {
+	$('body').on('click', '.dp-po-comments.dp-po-add-comments', function(event) {
 		$(this).toggleClass('is-active');
-		$('.dp-po-comments-wrap').slideToggle(200)
+		$(this).nextAll('.dp-po-comments-wrap').slideToggle(200)
+	})
+
+	$('body').on('click', '.dp-po-sort', function(event) {
+		$(this).toggleClass('is-active');
+		$(this)('.dp-po-dropdown-menu.Menu--tick').slideToggle(200)
 	})
 })
+
+// cut function subscribe btn
+
+function filterView_subscribe(){
+    if (window.matchMedia('(max-width: 767px)').matches) {
+        $('.Sidebar--right .dp-po-Dropdown-container.subscribe--dropdown').detach().appendTo('.dp-po-col-9 .mobileCutBtn');
+    }else{  
+        $('.mobileCutBtn .dp-po-Dropdown-container.subscribe--dropdown').detach().prependTo('.Sidebar--right');
+    }
+}
+
+// cut function Share btn
+
+function filterView_share(){
+    if (window.matchMedia('(max-width: 767px)').matches) {
+        $('.mobileCutShare .Dropdown-share').detach().appendTo('.dp-po-col-9 .mobileCutBtn');
+    }else{  
+        $('.mobileCutBtn .Dropdown-share').detach().prependTo('.Sidebar--right .mobileCutShare');
+    }
+}
+
+$(document).ready(function() {
+    filterView_subscribe();
+    filterView_share();
+    $(window).resize(function() {
+        filterView_subscribe();
+        filterView_share();
+    });
+});
