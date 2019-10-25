b609185121ad9c6fd3dd2f1b4fbcf35ad69d67ad
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-25 09:12:57 +0000

    Deploy deskpro/portal-components to github.com/deskpro/portal-components.git:gh-pages

--- demo/vendors~main.e18ff46a91742d3ec669.bundle.js
+++ demo/vendors~main.e18ff46a91742d3ec669.bundle.js
@@ -112,4 +112,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.f9bb4ac3acfbe36d50eb.bundle.js.map
+//# sourceMappingURL=vendors~main.e18ff46a91742d3ec669.bundle.js.map
