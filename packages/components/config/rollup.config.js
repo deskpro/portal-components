4754508f20075581a4db087a051dc24db15d05da
Julien Ducro <julien.ducro@deskpro.com>
2023-08-31 17:43:50 +0200

    Attempt to fix react-select styling

--- packages/components/config/rollup.config.js
+++ packages/components/config/rollup.config.js
@@ -2,11 +2,13 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import babel from "@rollup/plugin-babel";
 
 const packageJson = require('../package.json');
 
 export default {
   input:  'src/index.ts',
+  external: ['react', '@emotion/core'],
   output: [
     {
       file:      packageJson.main,
@@ -22,9 +24,15 @@ export default {
   plugins: [
     peerDepsExternal(),
     resolve(),
+    commonjs(),
     typescript({
       useTsconfigDeclarationDir: true
     }),
-    commonjs(),
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-env", "@babel/preset-react"],
+      extensions: [".js"],
+      exclude: ["node_modules/**"],
+    }),
   ]
 };
