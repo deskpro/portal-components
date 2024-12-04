73bd1dbec94b000307eb59de7d05e73f813dd81c
Julien Ducro <julien.ducro@deskpro.com>
2024-12-04 19:30:06 +0100

    Fix Unresolved dependencies

--- packages/components/config/rollup.config.js
+++ packages/components/config/rollup.config.js
@@ -1,3 +1,4 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 
@@ -19,6 +20,7 @@ export default {
     }
   ],
   plugins: [
+    nodeResolve(),
     typescript(),
     commonjs(),
   ]
