ede9b869fb8a99bc7c41a5cf606bdba8d7a2aabe
Julien Ducro <julien.ducro@deskpro.com>
2025-05-07 17:33:59 +0200

    Upgrade date-picker

--- packages/components/config/rollup.config.js
+++ packages/components/config/rollup.config.js
@@ -6,17 +6,27 @@ const packageJson = require('../package.json');
 
 export default {
   input:  'src/index.ts',
-  external: ['react', '@emotion/core', packageJson.peerDependencies],
+  external: ['react', '@emotion/core', '@floating-ui/react', packageJson.peerDependencies],
   output: [
     {
       file:      packageJson.main,
       format:    'cjs',
-      sourcemap: true
+      sourcemap: true,
+      globals: {
+        react: 'React',
+        '@emotion/core': '@emotion/core',
+        '@floating-ui/react': '@floating-ui/react'
+      }
     },
     {
       file:      packageJson.module,
       format:    'esm',
-      sourcemap: true
+      sourcemap: true,
+      globals: {
+        react: 'React',
+        '@emotion/core': '@emotion/core',
+        '@floating-ui/react': '@floating-ui/react'
+      }
     }
   ],
   plugins: [
